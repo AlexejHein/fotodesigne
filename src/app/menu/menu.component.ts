2cf710891bd273e2c36fec86ada5b3ad79feb757
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2025-02-23 09:37:55 +0100

    add mobile nav

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   standalone: false,
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class MenuComponent {
 
